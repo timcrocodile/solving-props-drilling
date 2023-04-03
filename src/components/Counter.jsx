11e23ea800478a204f1506e3533d0409ac797f35
timcrocodile <timcrocodile@proton.me>
2023-04-03 15:06:04 +0200

    jj

---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+const Counter = () => {
+  return (
+    <div className="Counter">
+      <button className="increment"> increment </button>
+      <h1>("0")</h1>
+      <button className="decrement"> decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
